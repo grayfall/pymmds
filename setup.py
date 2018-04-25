f84e0792f3118eafce0fdf4130ae3b4decf0eb28
Ilia Korvigo <ilia.korvigo@gmail.com>
2018-04-25 21:02:39 +0300

    Adding a release download link to setup.py

--- setup.py
+++ setup.py
@@ -12,11 +12,12 @@ setup(
     version='1.0',
     packages=['mmds'],
     url='https://github.com/grayfall/pymmds',
+    download_url='https://github.com/grayfall/pymmds/archive/1.0.tar.gz',
     license='MIT',
     author='Ilia Korvigo',
     author_email='ilia.korvigo@gmail.com',
     description='A Python package for active metric MDS.',
     install_requires=['numpy>=1.14.0', 'pandas>=0.22.0'],
-    keywords = ['multidimensional scaling', 'principal coordinate analysis',
-                'mds', 'landmark mds']
+    keywords=['multidimensional scaling', 'principal coordinate analysis',
+              'mds', 'landmark mds']
 )

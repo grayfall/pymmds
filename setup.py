de37f3ee897bfdfaa593a3f0c9ada4f49dcb70f9
Ilia Korvigo <ilia.korvigo@gmail.com>
2018-04-25 20:56:50 +0300

    Cleaning up for PyPI

--- setup.py
+++ setup.py
@@ -9,12 +9,14 @@ if sys.version_info < (3, 5):
 
 setup(
     name='pymmds',
-    version='1.0b',
+    version='1.0',
     packages=['mmds'],
-    url='',
+    url='https://github.com/grayfall/pymmds',
     license='MIT',
     author='Ilia Korvigo',
     author_email='ilia.korvigo@gmail.com',
     description='A Python package for active metric MDS.',
-    install_requires=['numpy>=1.14.0', 'pandas>=0.22.0']
+    install_requires=['numpy>=1.14.0', 'pandas>=0.22.0'],
+    keywords = ['multidimensional scaling', 'principal coordinate analysis',
+                'mds', 'landmark mds']
 )
